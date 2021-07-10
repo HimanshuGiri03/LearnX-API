de81ae9a91f08dba682e697ae89ea92cc14e80df
Shaurin <shaurin2000@gmail.com>
2021-07-10 17:14:37 +0530

    Connected application with Database and Added bootcamp Model

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-//Route files 
-const bootcamps = require('./routes/bootcampRoute');
+const connectDB = require('./config/db');
 
 //Load env vars 
 dotenv.config({ path : './config/config.env'});
+
+//Connect to Database
+connectDB();
+
+//Route files 
+const bootcamps = require('./routes/bootcampRoute');
 
 const app = express();
 
@@ -20,4 +24,11 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+
+//Unhandled Promise Rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error : ${err.message}`);
+    //Close Server and Exit Process
+    server.close(() => { process.exit(1)} );
+});
