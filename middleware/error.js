a19bb70c48b981b28baeee30099118dc0dd1c2f5
Shaurin <shaurin2000@gmail.com>
2021-07-21 17:03:27 +0530

    Added a few security prevention measures

--- middleware/error.js
+++ middleware/error.js
@@ -7,7 +7,7 @@ const errorHandler = (err, req, res, next) => {
 
     //Bad Id Format
     if(err.name === 'CastError'){
-        const message = `Resource not found with id of ${err.value}`;
+        const message = `Resource not found`;
         error = new ErrorResponse(message, 404);
     }
     //Duplicate Key Errror
